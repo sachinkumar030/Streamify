266e9f50caed4538c341751d67ada6ccc3d8ac3e
Sachin Kumar <sachinkuma.dev@gmail.com>
2026-07-06 12:03:17 +0530

    Add Vercel rewrite

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import "stream-chat-react/dist/css/v2/index.css";
 import "./index.css";
 import App from "./App.jsx";
-
-import { BrowserRouter } from "react-router";
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
